5e4694de3bec493d87b3ea21be7b5f5da78f8ee4
Adrian Aguero <iceberg.04@gmail.com>
2023-01-01 17:19:10 -0300

    [TAREA] sensei 2

--- ninja.js
+++ ninja.js
@@ -21,3 +21,25 @@ class Ninja {
     this.health += 10;
   }
 }
+
+
+class Sensei extends Ninja {
+  constructor(name) {
+    super(name,200);
+    this.speed = 10;
+    this.strength = 10;
+    this.wisdom = 10;
+  }
+
+  speakWisdom(){
+    console.log("You only live once");
+    super.drinkSake();
+  }
+}
+
+// ejemplo de salida
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
